0c96928e8e3d42453863c3d24be8c819c7bb2202
lidongyang <li_dong_yang@qq.com>
2020-10-17 19:25:04 +0800

    add config

--- spring-01-ioc/src/main/java/com/linkedbear/spring/environment/b_api/EnvironmentApiApplication.java
+++ spring-01-ioc/src/main/java/com/linkedbear/spring/environment/b_api/EnvironmentApiApplication.java
@@ -1,13 +1,13 @@
 package com.linkedbear.spring.environment.b_api;
 
 import com.linkedbear.spring.environment.b_api.bean.EnvironmentHolder;
+import com.linkedbear.spring.environment.b_api.config.EnvironmentPropertyConfiguration;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class EnvironmentApiApplication {
-    
+
     public static void main(String[] args) throws Exception {
-        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
-                );
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(EnvironmentPropertyConfiguration.class);
         EnvironmentHolder environmentHolder = ctx.getBean(EnvironmentHolder.class);
         environmentHolder.printEnvironment();
     }
